a63b844b9ec8f6946a9cba4a3d07f3822f914e65
Evren Ermiş <36535914+eermis1@users.noreply.github.com>
2020-07-18 22:19:50 +0300

    Process_data Bugfix for DB Save

--- data/process_data.py
+++ data/process_data.py
@@ -41,7 +41,7 @@ def clean_data(df):
 def save_data(df, database_filename):
 
     engine = create_engine('sqlite:///' + database_filename)
-    df.to_sql('clean_dataset', engine, index=False)
+    df.to_sql('clean_dataset', engine, index=False, if_exists= 'replace')
 
 
 def main():
